2354d4c005d6a8460c75d520fce157cb84c41244
jonathan-gu <jonathanguerpillon@gmail.com>
2022-12-28 16:11:58 +0100

    add function createElement for a better understanding of the code

---
+++ src/utils/createElement.js
@@ -0,0 +1,33 @@
+const createElement = ({ tagName, ...rest }) => {
+    const element = document.createElement(tagName)
+
+    if ("classList" in rest) {
+        element.classList.add(...rest.classList)
+    }
+
+    if ("text" in rest) {
+        element.textContent = rest.text
+    }
+
+    if ("attributes" in rest) {
+        for (let [key, value] of Object.entries(rest.attributes)) {
+            element.setAttribute(key, value)
+        }
+    }
+
+    if ("children" in rest) {
+        for (let children of createBulkElement(rest.children)) element.appendChil(children)
+    }
+
+    return element
+}
+
+export const createBulkElement = (elements) => {
+    const result = []
+    for (let element of elements) {
+        result.push(createElement(element))
+    }
+    return result
+}
+
+export default createElement
